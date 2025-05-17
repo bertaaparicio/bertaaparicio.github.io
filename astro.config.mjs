ad38dcb7d7728371e41163a9f3aacdfd97e0e0be
Luca Ubiali <luca.ubiali@gmail.com>
2025-05-17 05:29:14 +0200

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+
+import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    trailingSlash: "never",
+    site: "https://example.com",
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [mdx(), icon(), sitemap()],
+});
